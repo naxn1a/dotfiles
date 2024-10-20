9d54d91f33fe8d16e9e6673552ed71d6cd81ed68
Naxn1a <persecond.sp@gmail.com>
2024-10-21 03:29:46 +0700

    fix ts lsp

--- config/dot_config/nvim/lua/plugins/lsp.lua
+++ config/dot_config/nvim/lua/plugins/lsp.lua
@@ -44,7 +44,7 @@ return {
     require('mason').setup({})
     require('mason-lspconfig').setup({
       ensure_installed = {
-        'tsserver',
+        'ts_ls',
         'eslint',
         'rust_analyzer',
         'gopls',
