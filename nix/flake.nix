d226615a105f838cd402b98997da0167c2da33ce
naxn1a <naxn1a@naxn1as-MacBook-Air.local>
2024-10-24 21:03:18 +0700

    fix nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -57,16 +57,12 @@
       bun
 
       # Terminal utilities
-      kitty
       bat
       fzf
       ripgrep
       tree
       eza
       fd
-
-      # Utility
-      mkalias
     ];
 
     # Shared shell configuration
@@ -99,6 +95,7 @@
           "chezmoi"
         ];
         casks = [
+          "kitty"
           "hammerspoon"
           "docker"
           "raycast"
@@ -118,27 +115,6 @@
       fonts.packages = with pkgs; [
         (nerdfonts.override { fonts = [ "JetBrainsMono" "FiraMono" ]; })
       ];
-
-      # Activation script
-      system.activationScripts.applications.text = let
-        env = pkgs.buildEnv {
-          name = "system-applications";
-          paths = config.environment.systemPackages;
-          pathsToLink = "/Applications";
-        };
-      in
-        pkgs.lib.mkForce ''
-        # Set up applications.
-        echo "setting up /Applications..." >&2
-        rm -rf /Applications/Nix\ Apps
-        mkdir -p /Applications/Nix\ Apps
-        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-        while read src; do
-          app_name=$(basename "$src")
-          echo "copying $src" >&2
-          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-        done
-      '';
 
       # Darwin-specific system settings
       system.defaults = {
